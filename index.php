ae0163e3092402480c8b6b60304bcf2e0fd1f992
bunga <bungaasri760@gmail.com>
2025-09-01 10:42:34 +0700

    buat file index

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP dasar</title>
+</head>
+<body>
+    <?php $nama = "bunga"?>
+    <h1>selamat datang, <?php echo $nama; ?>!</h1>
+</body>
+</html>
