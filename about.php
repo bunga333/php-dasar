32c6e16bea7639a4b6f912a1a20210db2ff2f6c2
bunga <bungaasri760@gmail.com>
2025-09-02 08:40:06 +0700

    membuat file dan mengerjakan soal

--- about.php
+++ about.php
@@ -3,13 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php dasar</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php $nama = "bunga"; 
-          $umur ="16"
-    ?>
-    
-    <h1>selamat datang, <?php echo $nama; ?>!</h1>
+    ini halaman about
 </body>
 </html>
